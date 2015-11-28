20b661dcf064a9f6cd39755422217e2b05b7a6ca
oskarito <oskariot@wp.pl>
2015-11-28 23:19:10 +0100

    Forks added. Semaphores used

--- philo.c
+++ philo.c
@@ -1,64 +1,90 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <pthread.h>
+#include <semaphore.h>
 
-// Filozof
+int NUMBER_OF_ITERATIONS = 30;
+
+// Funkcje pomocnicze
+void delayRandom(void);
+
+// Filozofowie i sztućce
 enum { THINKING, HUNGRY, EATING };
+const char * philosophersNames[5] = { "Aristotle", "Socrates", "Zeno of Elea", "Plato", "Xenophanes" };
 
-void think(int philosopherId)
-{
-  printf("Philosopher %d is thinking...\n", philosopherId);
-  clock_t now = clock();
-  clock_t duration = (rand() % 5) * CLOCKS_PER_SEC; // losowa długość czynności
-  while (clock() < now + duration);
-}
+pthread_t Philosophers[5];
+sem_t Forks[5];
 
-void eat(int philosopherId)
+void putState(int id, const char * state)
 {
-  printf("Philosopher %d is eating...\n", philosopherId);
-  clock_t now = clock();
-  clock_t duration = (rand() % 5) * CLOCKS_PER_SEC; // losowa długość czynności
-  while (clock() < now + duration);
+  printf("%s is %s...\n", philosophersNames[id], state);
 }
 
 void * lifeOfThePhilosopher(void * args)
 {
-  int philosopherId = *((int *)args);
+  int id = *((int *)args);
   free(args);
-  while (1)
+  int i = 0;
+  while (i++ < NUMBER_OF_ITERATIONS)
   {
-    think(philosopherId);
-    // takeForks(philosopherId);
-    eat(philosopherId);
-    // dropForks(philosopherId);
+    putState(id, "thinking");
+    delayRandom();
+    putState(id, "hungry");
+    int left = id;
+    int right = (id + 1) % 5;
+    sem_wait(&Forks[id]);
+    sem_wait(&Forks[(id + 1) % 5]);
+    putState(id, "eating");
+    delayRandom();
+    sem_post(&Forks[id]);
+    sem_post(&Forks[id]);
   }
 }
 
+
 int main()
 {
+  int tret; // wartość zwracana przez różne pthread_funkcja()
   srand(time(NULL));
-  pthread_t Philosophers[5];
+
+  // inicjowanie semaforów
+  for (int i = 0; i < 5; i++)
+    sem_init(&Forks[i], 0, 1);
+
+
+
+  // startowanie wątków
   int * id;
-  int tret; // sprawdzenie, czy wątek utworzył się poprawnie
   for (int i = 0; i < 5; i++)
   {
     id = malloc(sizeof(int));
-    *id = i + 1;
+    *id = i;
     if (tret = pthread_create(&Philosophers[i], NULL, lifeOfThePhilosopher, (void *)id))
     {
       fprintf(stderr,"Error from pthread_create(). Returned %i instead of 0.", tret); // error code display
       exit(EXIT_FAILURE);
     }
   }
+
+  // kończenie wątków
   for (int i = 0; i < 5; i++)
-  {
     if (tret = pthread_join(Philosophers[i], NULL)) // joins back the pthread
     {
       fprintf(stderr, "Error from pthread_join(). Returned %i instead of 0.", tret); // error code display
       exit(EXIT_FAILURE);
     }
-  }
-  
+
+  // destrukcja semaforów
+  for (int i = 0; i < 5; i++)
+    sem_destroy(&Forks[i]);
   return 0;
+}
+
+// Funkcje pomocnicze
+void delayRandom()
+{
+  int random = rand();
+  while (random-- >= 1);
 }
